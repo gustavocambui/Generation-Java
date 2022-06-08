cee7280eabdc91d3cef97b87d35969bc22293f25
Gustavo <jgustavocambui@gmail.com>
2022-06-08 07:58:09 -0300

    Adição do exercício número 3

---
+++ exerciciosLista02/Exercicio03.java
@@ -0,0 +1,29 @@
+package exerciciosLista02;
+/*3- Faça um programa que receba a idade de uma pessoa e mostre na saída em qual
+categoria ela se encontra:
+ 10-14 infantil
+ 15-17 juvenil
+ 18-25 adulto */
+import java.util.Scanner;
+
+public class Exercicio03 {
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner (System.in);
+		int idade;
+		System.out.println("Digite sua idade:");
+		idade = leitor.nextInt();
+			if(idade>=10 && idade<=14) {
+				System.out.println("Categoria: infantil.");
+			}
+			else if(idade>=15 && idade<=17) {
+				System.out.println("Categoria: juvenil.");
+			}
+			else if(idade>=18 && idade<=25) {
+				System.out.println("Categoria: adulto.");
+			}
+			else {
+				System.out.println("Idade fora das categorias estabelecidas.");
+			}
+	}
+
+}
